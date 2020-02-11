eb2b7d1a633f1b9c419f463a5997a6fafbf95368
eloifournier <43586674+eloifournier@users.noreply.github.com>
2020-02-10 22:23:49 -0500

    Add files via upload

---
+++ devoir2.py
@@ -0,0 +1,45 @@
+# coding : utf-8
+
+import csv, json
+import requests
+
+fichier = "lobbying.csv"
+
+url = "http://jhroy.ca/uqam/lobby.json"
+
+entetes = {
+    "User-Agent":"Éloi Fournier - 5149732575 : Salut JH, j'essaie de réussir le devoir 2", 
+    "From":"eloifournier@gmail.com"
+}
+
+req = requests.get(url,headers=entetes)
+
+entreprises = req.json()
+# print(entreprises)
+
+for entreprise in entreprises["registre"]:
+    if req.status_code == 200:
+        # print("Yé")
+        infos = [] 
+        nb = entreprise[0]["comlog_id"]
+        nom = entreprise[0]["fr_client_org_corp_nm"]
+        name = entreprise[0]["en_client_org_corp_nm"]
+        date = entreprise[0]["date_comm"]
+        objet = entreprise[1][0]["objet"]
+        objet2 = entreprise[1][0]["objet_autre"]
+        institution = entreprise[2][0]["institution"]
+        infos.append(nb)
+        infos.append(nom)
+        infos.append(name)
+        infos.append(date)
+        infos.append(objet)
+        infos.append(objet2)
+        infos.append(institution)
+        if "limat" in objet or "limat" in objet2:  
+            print(infos)
+            dead = open(fichier,"a")
+            obies = csv.writer(dead)
+            obies.writerow(infos)       
+
+    # Mon fichier csv contient TOUTES les entreprises. J'ai essayé une multitude de solutions, mais j'avais soit toutes les entreprises ou un csv vide...
+    # Ok j'avais fait une petite erreur d'indentation mais ça marche finalement! :)
